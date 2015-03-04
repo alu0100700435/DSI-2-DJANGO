e1394112c22ecdfaadcd24f3bd270c2946a76e40
Raquel <alu0100700435@ull.edu.es>
2015-03-04 18:40:00 +0000

    add views

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    url(r'^', include('polls.urls')),
+    url(r'^', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
 )
